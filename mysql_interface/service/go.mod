1c53b01efcfcfd479ba9020d568525c5a673e44e
tree <807596654@qq.com>
2020-05-21 20:03:58 +0800

    add misc challenge 'mysql_interface'

---
+++ mysql_interface/service/go.mod
@@ -0,0 +1,41 @@
+module github.com/tr3ee/RCTF2020/mysql_interface
+
+go 1.13
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/fasthttp-contrib/websocket v0.0.0-20160511215533-1f3b11f56072 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/iris-contrib/jade v1.1.4 // indirect
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/kataras/golog v0.0.15 // indirect
+	github.com/kataras/iris/v12 v12.1.8
+	github.com/klauspost/compress v1.10.6 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/pingcap/log v0.0.0-20200511115504-543df19646ad // indirect
+	github.com/pingcap/parser v3.1.2-0.20200507065358-a5eade012146+incompatible
+	github.com/pingcap/tidb v1.1.0-beta.0.20200520024639-0414aa53c912
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/shirou/gopsutil v2.20.4+incompatible // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/valyala/fasthttp v1.12.0 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
+	github.com/yudai/pp v2.0.1+incompatible // indirect
+	go.uber.org/zap v1.15.0 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+	golang.org/x/tools v0.0.0-20200521211927-2b542361a4fc // indirect
+	gopkg.in/ini.v1 v1.56.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200506231410-2ff61e1afc86 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.4 // indirect
+)
---
+++ mysql_interface/service/mysql_interface.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/hex"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kataras/iris/v12"
+	"github.com/pingcap/parser"                     // v3.1.2-0.20200507065358-a5eade012146+incompatible
+	_ "github.com/pingcap/tidb/types/parser_driver" // v1.1.0-beta.0.20200520024639-0414aa53c912
+)
+
+var isForbidden = [256]bool{}
+
+const forbidden = "\x00\t\n\v\f\r`~!@#$%^&*()_=[]{}\\|:;'\"/?<>,\xa0"
+
+func init() {
+	for i := 0; i < len(forbidden); i++ {
+		isForbidden[forbidden[i]] = true
+	}
+}
+
+func allow(payload string) bool {
+	if len(payload) < 3 || len(payload) > 128 {
+		return false
+	}
+	for i := 0; i < len(payload); i++ {
+		if isForbidden[payload[i]] {
+			return false
+		}
+	}
+	if _, _, err := parser.New().Parse(payload, "", ""); err != nil {
+		return true
+	}
+	return false
+}
+
+func pow(ctx iris.Context) {
+	rwlock.RLock()
+	if ctx.FormValue("pow") != secret {
+		ctx.StatusCode(iris.StatusForbidden)
+		ctx.HTML("Error: <b> Wrong pow </b>")
+		rwlock.RUnlock()
+		return
+	}
+	rwlock.RUnlock()
+	ctx.Next()
+}
+
+func sqlHandler(ctx iris.Context) {
+	sql := ctx.FormValue("sql")
+	if allow(sql) {
+		var result string
+		rows, err := db.Query(sql)
+		if err != nil {
+			ctx.HTML(err.Error())
+			return
+		}
+		defer rows.Close()
+		if rows.Next() {
+			if err := rows.Scan(&result); err != nil {
+				ctx.HTML(err.Error())
+				return
+			}
+			ctx.HTML(result)
+			return
+		}
+		ctx.HTML("Empty set")
+		return
+	}
+	ctx.StatusCode(iris.StatusForbidden)
+	ctx.HTML("Error: <b> Not allowed </b>")
+}
+
+const (
+	prefix  = "RCTF2020_mysql_interface_"
+	letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
+var (
+	db        *sql.DB
+	rwlock    sync.RWMutex
+	secret    = "000"
+	challenge = hash(prefix + secret)
+)
+
+func hash(raw string) string {
+	hash := sha256.Sum256([]byte(raw))
+	return hex.EncodeToString(hash[:])
+}
+
+func main() {
+	rand.Seed(time.Now().Unix() ^ 0xcafebabe591591)
+	ticker := time.NewTicker(60 * time.Second)
+	go func() {
+		for range ticker.C {
+			next := ""
+			for i := 0; i < 3; i++ {
+				next += string(letters[rand.Intn(len(letters))])
+			}
+			rwlock.Lock()
+			secret = next
+			challenge = hash(prefix + secret)
+			rwlock.Unlock()
+		}
+	}()
+	defer ticker.Stop()
+
+	var err error
+	db, err = sql.Open("mysql", "mysql_interface:b41fec9c1bcb194fb2028fa43dd74722@tcp(mysqld:3306)/mysql_interface")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	app := iris.New()
+	app.HandleDir("/assets", "./assets")
+	app.RegisterView(iris.HTML("./templates", ".html"))
+	app.Get("/", func(ctx iris.Context) {
+		rwlock.RLock()
+		ctx.ViewData("prefix", prefix)
+		ctx.ViewData("challenge", challenge)
+		rwlock.RUnlock()
+		ctx.View("index.html")
+	})
+
+	app.Post("/", pow, sqlHandler)
+
+	if err := app.Listen(":8081", iris.WithPostMaxMemory(1)); err != nil {
+		log.Fatal(err)
+	}
+}
